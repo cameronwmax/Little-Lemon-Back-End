d1fefe80d88aa376019396626a4f48bfb863e0e7
Cameron Maxwell <cameronwmax@icloud.com>
2025-02-24 11:31:50 -0800

    Final commit

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from restaurant.views import BookingViewSet
+from restaurant.views import BookingViewSet, index
+
 
 
 router = DefaultRouter()
@@ -26,6 +27,7 @@ router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='inedx'),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/menu/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
